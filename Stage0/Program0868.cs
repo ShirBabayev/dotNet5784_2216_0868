ed457d972364c728baf37ee8758a84ea687eaced
RenanaHouri <renanahouri69@gmail.com>
2023-12-28 20:19:24 +0200

    adding welcome function of the second partner

---
+++ Stage0/Program0868.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0;
+
+partial class Program0868
+{
+    private static void welcome0868()
+    {
+        Console.WriteLine("I am also here!");
+    }
+}
