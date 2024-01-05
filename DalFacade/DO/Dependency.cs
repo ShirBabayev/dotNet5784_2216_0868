ab005776f7e09996aeea2afd207bc5e57c8da128
RenanaHouri <renanahouri69@gmail.com>
2024-01-05 13:04:23 +0200

    add Initialization

--- DalFacade/DO/Dependency.cs
+++ DalFacade/DO/Dependency.cs
@@ -3,6 +3,17 @@
 public record Dependency
 (
    int Id,///running identifier add something
-   int dependentTask,
-    int formerTask
-    );
+   int? dependentTaskId=null,
+    int? formerTaskId=null
+    )
+{
+    public Dependency(int id, int dependentTaskId, int formerTaskId) : this()
+    {
+        this.Id = id;
+        this.dependentTaskId = dependentTaskId;
+        this.formerTaskId = formerTaskId;
+    }
+
+    public Dependency() : this(0) { } //empty ctor for stage 3
+
+}
--- DalFacade/DO/Task.cs
+++ DalFacade/DO/Task.cs
@@ -1,21 +1,41 @@
 ﻿namespace DO;
 
 public record Task
-(       
+(
         int Id,///running identifier add something
         string nickName,
         string description,
         bool mileStone,
-        DateTime dateOfCreation,
-        DateTime PlanedDateOfstratJob,
-        DateTime dateOfstratJob,//the date when the mission was actually started
-        TimeSpan durationOfTask,
-        DateTime deadline,
-        DateTime dateOfFinishing,//the date when the mission was actually done
         string deliverables,//products???
+        DifficultyLevel levelOfDifficulty,
         int engineerId,
-        int levelOfDifficulty
+        DateTime? dateOfCreation = null,
+        DateTime? PlanedDateOfstratJob = null,
+        DateTime? dateOfstratJob = null,//the date when the mission was actually started
+        TimeSpan? durationOfTask = null,
+        DateTime? deadline = null,
+        DateTime? dateOfFinishing = null//the date when the mission was actually done
     )
 {
+    public Task(int Id, string nickName, string description,
+        bool mileStone, DateTime dateOfCreation, DateTime PlanedDateOfstratJob, DateTime dateOfstratJob,
+        TimeSpan durationOfTask, DateTime deadline, DateTime dateOfFinishing, string deliverables,
+        int engineerId, DifficultyLevel levelOfDifficulty) : this()
+    {
+        this.Id=Id;
+        this.nickName=nickName;
+        this.description=description;
+        this.mileStone=mileStone;
+        this.dateOfCreation=dateOfCreation;
+        this.PlanedDateOfstratJob=PlanedDateOfstratJob;
+        this.dateOfstratJob=dateOfstratJob;
+        this.durationOfTask=durationOfTask;
+        this.deadline=deadline;
+        this.dateOfFinishing=dateOfFinishing;
+        this.deliverables=deliverables;
+        this.engineerId=engineerId;
+        this.levelOfDifficulty=levelOfDifficulty;
+    }
 
+    public Task() : this(0, null, null, false, null, 0, 0) { } //empty ctor for stage 3
 }
