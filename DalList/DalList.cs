3a6b5efae45cd7901b45dc9dd6aaec2c02568d09
שיר באבאייב <hhhbdiha@gmail.com>
2024-01-29 19:33:15 +0200

    stage4 level3 update

--- DalList/DalList.cs
+++ DalList/DalList.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace Dal;
 
-sealed public class DalList : IDal
+sealed internal class DalList : IDal
 {
+    public static IDal Instance { get; } = new DalList();
+    private DalList() { }
     public IEngineer Engineer => new EngineerImplementation();
     public ITask Task =>  new TaskImplementation();
     public IDependency Dependency => new DependencyImplementation();
--- DalXml/DalXml.cs
+++ DalXml/DalXml.cs
@@ -1,14 +1,18 @@
 ﻿using DalApi;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Dal;
 
-sealed public class DalXml : IDal
+sealed internal class DalXml : IDal
 {
+    public static IDal Instance { get; } = new DalXml();
+    private DalXml() { }
+
     public IEngineer Engineer => new EngineerImplementation();
     public ITask Task => new TaskImplementation();
     public IDependency Dependency => new DependencyImplementation();
