3ac0eca42516d1b47c1b8f1b4bad4e3382314e01
RenanaHouri <renanahouri69@gmail.com>
2024-02-28 18:16:44 +0200

    after engineer's window desine-middle stage 5

--- PL/Engineer/EngineerWindow.xaml.cs
+++ PL/Engineer/EngineerWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace PL.Engineer
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_2(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_3(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_4(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
